225c88ad4f20cd529eb93204328b336fa0fbadbe
DesktopPC <mpalacz00@gmail.com>
2022-04-07 23:32:05 +0200

    Sorting histogram based on the value and printing it into file

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-histogram = {} # histogram dictionary
+def HistogramToString(histogram):
+    text = ''
+    for key in histogram.keys():
+        text = text + key + ' -> ' + str(histogram[key]) + '\n'
+    return text
 
+
+histogram = {}  # histogram dictionary
+directory = input("Input file directory: ")  # asking user to input file directory
 try:
-    file = open('input.txt', 'rt') # opening file
-    content = file.read().upper() # reading file and changing all letters to upper
-    for ch in content: # iterating through file content
-        if ch.isalpha(): # checking is character a letter
-            if ch in histogram.keys(): # checking is character within keys
-                histogram[ch] += 1 # rising counter value
+    file = open(directory, 'rt')  # opening file
+    content = file.read().upper()  # reading file and changing all letters to upper
+    for ch in content:  # iterating through file content
+        if ch.isalpha():  # checking is character a letter
+            if ch in histogram.keys():  # checking is character within keys
+                histogram[ch] += 1  # rising counter value
             else:
-                histogram[ch] = 1 # creating new key and setting counter value on 1
-    file.close() # closing file
+                histogram[ch] = 1  # creating new key and setting counter value on 1
+    file.close()  # closing file
+    # sorting histogram the highest amount of characters to lowest
+    histogram = dict(sorted(histogram.items(), key=lambda i: i[1], reverse=True))
+    file = open(directory + '.hist', 'wt')
+    file.write(HistogramToString(histogram))
+    file.close()
 except Exception as e:
     print(e)
 
-for key in histogram.keys():
-    print(key, '->', histogram[key]) # printing histogram
+print(HistogramToString(histogram))  # printing histogram
